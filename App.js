5c62c5a348229e8a39c905572f99820fa7a75118
esmasigirtmac <64590928+esmasigirtmac@users.noreply.github.com>
2020-12-13 00:54:56 +0300

    hello eklendi

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
