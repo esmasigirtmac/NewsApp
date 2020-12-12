d9dd52e0db778a16e15c36aa508cb471c2883b9a
husneiremkaya <ireemkaayaaa@gmail.com>
2020-12-13 00:57:56 +0300

    Merhaba eklendi
    ....

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Merhaba</Text>
       <StatusBar style="auto" />
     </View>
   );
